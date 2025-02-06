e1d447053b11b863494d9e8899533cb7b234a5bb
hyeonseokhan <toycode727@gmail.com>
2025-02-06 10:48:52 +0900

    최초 업로드

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type {NextConfig} from "next";
 
+const basePath = '/web-hosting-test';
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath,
+  env: {
+    BASE_PATH: basePath,
+  },
   output: 'export',
   distDir: 'docs',
 };
